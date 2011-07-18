ab7895c393783affb45525cd2f10fce9ab7667d3
Hariolf Haefele <hariolf@iplantcollaborative.org>
2011-07-18 15:15:53 -0700

    apply new code styling and formatting

--- src/main/java/org/iplantc/core/broadcaster/shared/BroadcastCommand.java
+++ src/main/java/org/iplantc/core/broadcaster/shared/BroadcastCommand.java
@@ -1,13 +1,12 @@
 package org.iplantc.core.broadcaster.shared;
 
 /**
- * This is the command for pushing json messages from adapters to the outer container.
- * These messages will then be "broadcast" to other adapters sharing the container.
+ * This is the command for pushing json messages from adapters to the outer container. These messages
+ * will then be "broadcast" to other adapters sharing the container.
  * 
  * @author amuir
  * 
  */
-public interface BroadcastCommand
-{
-	void broadcast(String jsonMsg);
+public interface BroadcastCommand {
+    void broadcast(String jsonMsg);
 }
--- src/main/java/org/iplantc/core/broadcaster/shared/Broadcaster.java
+++ src/main/java/org/iplantc/core/broadcaster/shared/Broadcaster.java
@@ -1,13 +1,12 @@
 package org.iplantc.core.broadcaster.shared;
 
 /**
- * Adapter interface for facilitating communication between different components. There is
- * a conceptual channel where components for passing messages between adapters.
+ * Adapter interface for facilitating communication between different components. There is a conceptual
+ * channel where components for passing messages between adapters.
  * 
  * @author amuir
  * 
  */
-public interface Broadcaster
-{
-	void setBroadcastCommand(BroadcastCommand cmdBroadcast);
+public interface Broadcaster {
+    void setBroadcastCommand(BroadcastCommand cmdBroadcast);
 }
